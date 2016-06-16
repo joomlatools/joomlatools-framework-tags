c6e34a27eb2355068d63d5f3782a26d4e19c12ff
Ercan Ozkaya <ercan@timble.net>
2016-06-16 13:12:35 +0300

    Revert "re #3: Make slug the default value field for listboxes"
    This reverts commit e834caa0aba027b20af735756b786b9d481550a7.

--- template/helper/listbox.php
+++ template/helper/listbox.php
@@ -31,7 +31,7 @@ class ComTagsTemplateHelperListbox extends KTemplateHelperListbox
             'entity'   => null,
             'filter'   => array(),
             'name'     => 'tags',
-            'value'    => 'slug',
+            'value'    => 'title',
             'prompt'   => false,
             'deselect' => false,
             'attribs'  => array(
